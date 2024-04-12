64d1922d3df9713ad8f707dd2603038815d31339
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2024-04-11 19:12:22 -0500

    Que sea lo que dios quiera

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,22 @@
 INSERT INTO CONFIGURATION (PROPIEDAD, VALOR) VALUES ('1', 'Valor 1');
+--Ejemplo
+
 --INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('1','a','porque aja');
+
+--Parcial
+
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('1','b','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('2','c','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('3','d','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('4','a','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('5','c','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('6','c','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('7','a','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('8','d','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('9','a','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('10','b','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('11','c','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('12','d','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('13','d','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('14','a','');
+--INSERT INTO juan_fernandez (pregunta,letraResp,argumento) VALUES ('15','c','');
